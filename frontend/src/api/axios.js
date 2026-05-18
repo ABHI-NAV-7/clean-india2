23e1e00d59efa7195652d9eecc0e1441cd7abc80
ABHI-NAV-7 <abhinaverolla7@gmail.com>
2026-05-18 12:19:54 +0530

    Connected live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,15 @@
-import axios
-from "axios";
+import axios from "axios";
 
-const API =
-axios.create({
+
+
+const API = axios.create({
 
   baseURL:
-  "https://clean-india2.onrender.com"
+
+  "https://https://clean-india2.onrender.com"
 
 });
+
+
 
 export default API;
